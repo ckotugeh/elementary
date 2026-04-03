691da0b81d4c6ec9796dba3079cd30b9ab5bea6e
cotugeh <kotugeh@gmail.com>
2026-04-03 16:10:23 +0300

    test_cases

--- main.go
+++ main.go
@@ -5,12 +5,17 @@ import (
 )
 
 func main() {
-	// fmt.Println(CountAlpha("Hello world"))
-	// fmt.Println(CountAlpha("H e l l o"))
-	// fmt.Println(CountAlpha("H1e2l3l4o"))
-	// fmt.Println(FirstWord("Hello, ...Word"))
-	fmt.Println(FirstWord("Hello world"))
-	fmt.Println(FirstWord("H e l l o"))
-	fmt.Println(FirstWord("H1e2l3l4o"))
-	fmt.Println(FirstWord("Hello, ...Word"))
+	fmt.Println(CountAlpha("Hello world"))
+	fmt.Println(CountAlpha("H e l l o"))
+	fmt.Println(CountAlpha("H1e2l3l4o"))
+	
+	fmt.Println(FirstWord("   hello world")) // outputs: hello
+    fmt.Println(FirstWord("Go is awesome")) // outputs: Go
+    fmt.Println(FirstWord("    "))          // outputs: (empty)
+    fmt.Println(FirstWord(""))              // outputs: (empty)
+	fmt.Println(LastWord("hello world"))       // outputs: world
+    fmt.Println(LastWord("Go is awesome"))     // outputs: awesome
+    fmt.Println(LastWord("   multiple spaces ")) // outputs: spaces
+    fmt.Println(LastWord(""))                  // outputs: (empty))
+
 }
