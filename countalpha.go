5a45d5acb814005a868cabe8a01ba17f935d2a47
cotugeh <kotugeh@gmail.com>
2026-04-03 14:44:24 +0300

    Add count alpha

---
+++ countalpha.go
@@ -0,0 +1,42 @@
+package main
+
+func CountAlpha(s string) int {
+	count := 0
+	for _, v := range s {
+		
+		if v >= '0' && v <= '9' || v >= 'A' && v <= 'Z' || v >= 'a' && v <= 'z' {
+			count++
+		}
+	}
+	return count
+}
+
+/*
+Instructions
+Write a function CountAlpha() that takes a string as an argument and returns the number of alphabetic characters in the string.
+
+Expected functions
+func CountAlpha(s string) int {
+
+}
+Usage
+Here is a possible program to test your function:
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(CountAlpha("Hello world"))
+	fmt.Println(CountAlpha("H e l l o"))
+	fmt.Println(CountAlpha("H1e2l3l4o"))
+}
+And its output:
+
+$ go run .
+10
+5
+5
+*/
